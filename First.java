3cec0c473c00d30243f4d25134ae4fd9b2329bfb
AdityaKumar3112 <chvbaditya@gmail.com>
2021-05-03 21:21:44 +0530

    [Srikar | Yash] Add. static function to add two numbers

--- First.java
+++ First.java
@@ -1,6 +1,12 @@
 import java.io.*;
 public class First {
 	public static void main(String[] args) {
+		add(1,2);
 		System.out.println("Hellloooooo"); 
+	}
+
+	static void add (int a, int b) {
+		int c = a+b;
+		System.out.println(c);
 	}
 }
